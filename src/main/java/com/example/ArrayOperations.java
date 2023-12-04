31f94406ff00a46126391c84112eb7e0e3db83b0
Kaoru-kubota-Karura <cosmo.kaoru1211@gmail.com>
2023-12-04 12:32:02 +0800

    jaeger"Your forget that?"

--- src/main/java/com/example/ArrayOperations.java
+++ src/main/java/com/example/ArrayOperations.java
@@ -4,18 +4,35 @@ public class ArrayOperations {
     // Exercise 1: Find the Maximum Value in an Array
     public static int findMaximumValue(int[] array) {
         // TODO: Implement the logic to find the maximum value in the array.
-        return 0; // Placeholder return value
+        int max = array[0];
+        for (int value : array) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;// Placeholder return value
     }
 
     // Exercise 2: Calculate the Average of Array Elements
     public static double calculateAverage(int[] array) {
         // TODO: Implement the logic to calculate the average of the array elements.
-        return 0.0; // Placeholder return value
+        int sum = 0;
+        for (int value : array) {
+            sum += value;
+        }
+        return (double) sum / array.length;
     }
+
+
 
     // Exercise 3: Reverse an Array
     public static int[] reverseArray(int[] array) {
         // TODO: Implement the logic to reverse the array.
-        return new int[0]; // Placeholder return value
+        int[] reversedArray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            reversedArray[i] = array[array.length - 1 - i];
+        }
+        return reversedArray;
     }
+
 }
